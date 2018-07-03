9275d1d82de348798069b169a934336cfc786653
shiveshnavin <shiveshnavin@gmail.com>
2018-07-03 18:26:56 +0530

    Adde si2

--- src/empty_lib.c
+++ src/empty_lib.c
@@ -4,7 +4,7 @@
 bool is2(int no)
 {
 
-snprintf("Hello From Library.is2");
+printf("Hello From Library.is2");
 return no==2;
 
 }
@@ -12,7 +12,7 @@ return no==2;
 
 bool mgos_empty_lib_init(void) {
 
-snprintf("Hello From Library");
+printf("Hello From Library");
 
   LOG(LL_INFO, ("%s", "Hello From Library"));
   return true;
