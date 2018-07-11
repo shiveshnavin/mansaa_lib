1dacecc050f21c4d2e664d9953caff1ce31506e7
shiveshnavin <shiveshnavin@gmail.com>
2018-07-12 01:25:15 +0530

    AA

--- src/mansaa_lib.c
+++ src/mansaa_lib.c
@@ -9,6 +9,7 @@ return no==2;
 
 }
 
+ 
 
 bool mgos_mansaa_lib_init(void) {
 
@@ -22,10 +23,47 @@ printf("Hello From Library");
 	mgos_gpio_set_mode(19, MGOS_GPIO_MODE_OUTPUT);
 
 
-			mgos_pwm_set(4,200,0.1);
+			//mgos_pwm_set(4,200,0.1);
 			//mgos_pwm_set(16,200,0.1);
 			//mgos_pwm_set(5,200,0.1);
 			//mgos_pwm_set(19,200,0.1);
+
+				int ri,gi,bi,wi;
+				char *content = json_fread("userData.json");
+				LOG(LL_INFO, ("%s", content));
+				json_scanf(content, strlen(content), "{count: %d, led_r: %d , led_g: %d , led_b:%d , led_w:%d}", &count,  &ri,&gi,&bi,&wi  );
+				free(content);  
+
+			double r,g,b,w;
+			int FREQ=800;
+			r=ri/255.0;
+			g=gi/255.0;
+			b=bi/255.0;
+			w=wi/255.0; 
+			if(r<0)
+				r=0;
+			if(g<0)
+				g=0;
+			if(b<0)
+				b=0;
+			if(w<0)
+				w=0;
+			if(r>1)
+				r=1;
+			if(g>1)
+				g=1;
+			if(b>1)
+				b=1;
+			if(w>1)
+				w=1;
+			//if(w==0)
+			//	w=g;
+			
+
+			mgos_pwm_set(4,FREQ,r);
+			mgos_pwm_set(16,FREQ,g);
+			mgos_pwm_set(5,FREQ,b);
+			mgos_pwm_set(19,FREQ,w);
 
 
 
